dc8fc9cd66855f062d0cf4daf45c69d22fd2967b
Zid Visda <rashidvisda@gmail.com>
2025-07-11 22:57:24 +0800

    fixed sponsors

--- src/assets/components/Sponsors.jsx
+++ src/assets/components/Sponsors.jsx
@@ -5,13 +5,42 @@ import booking from "../images/icons/booking-icon.svg";
 const Sponsors = () => {
   return (
     <div className="bg-[#f4f0ee]">
-      <div className="custom-container flex w-full flex-col items-center justify-center bg-[#f4f0ee] md:justify-evenly lg:flex-row">
-        <Logo />
-        <div className="flex flex-row items-center justify-evenly lg:flex-row">
-          <a href="https://www.booking.com/" className="w-1/3" target="_blank">
+      <div className="custom-container flex w-full flex-col items-center bg-[#f4f0ee] pb-4 lg:flex-row">
+        <div className="flex flex-row items-center justify-evenly">
+          <Logo iconSize={4} textSize={8} />
+          {/* flexed in desktop, hidden mobile */}
+          <>
+            <a
+              href="https://www.booking.com/"
+              className="hidden w-1/3 lg:block lg:w-1/6"
+              target="_blank"
+            >
+              <img className="w-full" src={tripadvisor} alt="" />
+            </a>
+            <a
+              href="https://www.booking.com/"
+              className="hidden w-1/4 lg:block lg:w-1/7"
+              target="_blank"
+            >
+              <img className="w-full" src={booking} alt="" />
+            </a>
+          </>
+        </div>
+
+        {/* next row in desktop hidden in mobile */}
+        <div className="flex flex-row items-center justify-evenly lg:hidden">
+          <a
+            href="https://www.booking.com/"
+            className="w-1/3 lg:w-1/8"
+            target="_blank"
+          >
             <img className="w-full" src={tripadvisor} alt="" />
           </a>
-          <a href="https://www.booking.com/" className="w-1/3" target="_blank">
+          <a
+            href="https://www.booking.com/"
+            className="w-1/3 lg:w-1/7"
+            target="_blank"
+          >
             <img className="w-full" src={booking} alt="" />
           </a>
         </div>
